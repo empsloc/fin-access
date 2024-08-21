11c13fa31e4196696af7d29b24605a95c8e9d03c
empsloc <empsloc@gmail.com>
2024-08-21 17:17:12 +0530

    name issues fixed

--- components/sharedUI/Hero.tsx
+++ components/sharedUI/Hero.tsx
@@ -1,11 +1,14 @@
+"use client"
+import { useUser } from "@clerk/nextjs"
 import Image from "next/image"
 
 const Hero = () => {
+  const { isSignedIn, user, isLoaded } = useUser()
   return (
     <div className="w-full bg-secondary p-5 flex items-center rounded-xl">
         <div className=""><Image src="/hero.png" alt="" width={100} height={100}/> </div>
         <div className="flex flex-col">
-              <div className="font-semibold text-lg">  Hello Atharva, Welcome</div>
+              <div className="font-semibold text-lg">  Hello {user?.username}, Welcome</div>
               <div className="font-medium text-sm text-gray-700">Your onestop solution for visualising your financial data through safe and accurate representation</div>
             </div>
     </div>
